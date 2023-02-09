dc2c17fb464c9753aaffa2288c9a5f62e64dc18c
geoafrikana <geoafrikana@gmail.com>
2023-02-09 16:39:32 +0100

    web mercator. refactor load module

--- backend/shops/load.py
+++ backend/shops/load.py
@@ -1,48 +1,40 @@
-fp = r"C:\Users\USER\Desktop\laborratehero\GIS\shops_test.csv"
+# fp = r"C:\Users\USER\Desktop\laborratehero\GIS\shops_test.csv"
 import csv
-from .models import Shop
-from django.contrib.gis.geos import GEOSGeometry
+from .models import Shop, ZipCodes
+from django.contrib.gis.geos import Point
 
 
-with open(fp, 'r') as file:
-  csvreader = csv.reader(file)
-  i = 0
-  for row in csvreader:
-    if row[0] == 'WKT':
-      print(row)
-      i += 1
-    else:
-      s = Shop.objects.get(id=i)
-      los_angeles_epsg = 26911
-      s.geom_m = s.geom.transform(los_angeles_epsg, clone=True)
-      s.save()
-      print(s.geom_m)
-      i += 1
-      # a = Shop(name=row[4], lat=round(float(row[2]), 2),
-      #           lon=round(float(row[3]), 2) ,geom=row[0],
-      #            geom_m=GEOSGeometry(f'SRID=26911;{row[0]}'))
-      # print(a.geom, a.geom_m)
-      # a.save()
+def load_shop(fp):
+  with open(fp, 'r') as file:
+    data = csv.reader(file)
+    i = 0
+    for row in data:
+      if row[0] == 'id':
+        print(row)
+      else:
+        name=row[1]
+        lat_y, lon_x = float(row[2]), float(row[3])
+        loc_4326 = Point(x=lon_x, y=lat_y, srid=4326)
+        loc_1324 = loc_4326.transform(3857, clone=True) # EPSG:3857 is web mercator
+        s = Shop(name=name, lat=lat_y, lon=lon_x , geom=loc_4326, geom_m=loc_1324)
+        s.save()
+        print(s.geom_m)
 
-# fp = r"C:\Users\USER\Desktop\laborratehero\GIS\us_zipcodes.csv"
-# import csv
-# from .models import ZipCodes
-# from django.contrib.gis.geos import GEOSGeometry, Point
-
-# with open(fp, 'r') as file:
-#   data = csv.reader(file)
-#   for row in data:
-#     if row[0] == 'ZIP':
-#       continue
-#     else:
-#       x=float(row[2])
-#       y=float(row[1])
-#       x = round(x, 6)
-#       y= round(y,6)
-#       point = Point(x=x, y=y, srid=4326)
-#       los_angeles_srid = 26911
-#       point.transform(los_angeles_srid)
-#       z = ZipCodes(ZIP=row[0],
-#       lat=row[1], lon=row[2], geom=point)
-#       z.save()
-#   print('done')
+def load_zip_codes(fp):
+  with open(fp, 'r') as file:
+    data = csv.reader(file)
+    for row in data:
+      if row[0] == 'ZIP':
+        continue
+      else:
+        x=float(row[2])
+        y=float(row[1])
+        x = round(x, 6)
+        y= round(y,6)
+        point = Point(x=x, y=y, srid=4326)
+        point.transform(3857)
+        z = ZipCodes(ZIP=row[0],
+        lat=row[1], lon=row[2], geom=point)
+        print(z)
+        z.save()
+    print('done')
--- backend/shops/models.py
+++ backend/shops/models.py
@@ -5,7 +5,7 @@ class Shop(models.Model):
     lat = models.DecimalField(max_digits=5, decimal_places=2)
     lon = models.DecimalField(max_digits=5, decimal_places=2)
     geom  = models.PointField(srid=4326)
-    geom_m = models.PointField(srid=26911)
+    geom_m = models.PointField(srid=3857) # web mercator
 
     def __str__(self):
         return self.name
@@ -14,7 +14,7 @@ class ZipCodes(models.Model):
     ZIP = models.CharField(max_length=6)
     lat = models.DecimalField(max_digits=10, decimal_places=6)
     lon = models.DecimalField(max_digits=10, decimal_places=6)
-    geom = models.PointField(srid=26911)
+    geom = models.PointField(srid=3857)
 
     def __str__(self) -> str:
         return f"{self.ZIP} {self.geom}"
