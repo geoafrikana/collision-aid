9fddb5d476e81a5ae17d37fd821525d7530dcc9d
geoafrikana <geoafrikana@gmail.com>
2023-02-09 18:22:46 +0100

    static_root

--- backend/collision_aid_backend/settings.py
+++ backend/collision_aid_backend/settings.py
@@ -116,6 +116,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = config('STATIC_URL')
+STATIC_ROOT = BASE_DIR / config('STATIC_URL')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
